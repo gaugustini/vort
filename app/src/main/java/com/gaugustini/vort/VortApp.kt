d991c6310526685b16de8af170f2a63d900562e2
Gustavo Augustini <63506279+gaugustini@users.noreply.github.com>
2022-07-03 17:23:14 -0300

    Enable Light/Dark theme option

--- app/src/main/java/com/gaugustini/vort/VortApp.kt
+++ app/src/main/java/com/gaugustini/vort/VortApp.kt
@@ -1,6 +1,8 @@
 package com.gaugustini.vort
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
 import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
@@ -8,7 +10,26 @@ import dagger.hilt.android.HiltAndroidApp
 class VortApp : Application() {
 
     override fun onCreate() {
+        theme()
         DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+
+    private fun theme() {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val value = preferences.getString("theme", "system")
+        setAppTheme(value)
+    }
+
+    companion object {
+
+        fun setAppTheme(value: String?) {
+            when (value) {
+                "light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                "dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/gaugustini/vort/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/gaugustini/vort/ui/setting/SettingFragment.kt
@@ -2,8 +2,11 @@ package com.gaugustini.vort.ui.setting
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.gaugustini.vort.R
+import com.gaugustini.vort.VortApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,6 +15,15 @@ class SettingFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
         setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val theme = findPreference<ListPreference>("theme")
+        theme?.setOnPreferenceChangeListener { _, newValue ->
+            VortApp.setAppTheme(newValue.toString())
+            true
+        }
     }
 
     override fun onPrepareOptionsMenu(menu: Menu) {
